09ca5732cc90f3d4828db797c525f54233e4a24a
Fatemeh Chamani <fch199674@yahoo.com>
2025-08-04 14:43:14 +0330

    changes added

--- config/urls.py
+++ config/urls.py
@@ -23,5 +23,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', TemplateView.as_view(template_name="home.html"), name="home"),
+    path('', include('expenses.urls')),
     path('accounts/', include('allauth.urls')),
 ] + debug_toolbar_urls()
--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import Category, Budget, Expense
+
+
+@login_required
+def dashboard(request):
+    user = request.user
+    expenses = Expense.objects.filter(user=user)
+    budgets = Budget.objects.filter(user=user)
+    context = {
+        'expenses': expenses,
+        'budgets': budgets,
+    }
+    return render(request, 'expenses/dashboard.html', context)
