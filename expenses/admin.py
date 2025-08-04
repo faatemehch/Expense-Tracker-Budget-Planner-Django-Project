d2699a1c6195c001dcf712cba0d5415124490e15
Fatemeh Chamani <fch199674@yahoo.com>
2025-08-04 12:47:10 +0330

    expenses admin updated

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from expenses.models import Expense, Category, Budget
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'user',)
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'category', 'date', 'user', 'created_at', 'updated_at')
+
+
+@admin.register(Budget)
+class BudgetAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'category', 'period', 'user', 'created_at', 'updated_at')
