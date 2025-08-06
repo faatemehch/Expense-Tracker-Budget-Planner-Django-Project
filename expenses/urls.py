930ed95255bc02fbe66d726f15a644bbf82f5ef5
Fatemeh Chamani <fch199674@yahoo.com>
2025-08-06 07:59:01 +0330

    expenses views updated

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
 from . import views
+
 app_name = 'expenses'
 
 urlpatterns = [
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path('expenses/dashboard/', views.dashboard, name='dashboard'),
+    path('expenses/add/', views.add_expense, name='add_expense'),
+
+    path('expenses/list/', views.ExpenseListView.as_view(), name='expense_list'),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-from django.contrib.auth import get_user_model
+from django.shortcuts import render, redirect
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.decorators import login_required
+from django.views import generic
+from django.contrib import messages
 
 from .models import Category, Budget, Expense
+from .forms import ExpenseForm
 
 
 @login_required
@@ -16,3 +18,29 @@ def dashboard(request):
         'budgets': budgets,
     }
     return render(request, 'expenses/dashboard.html', context)
+
+
+class ExpenseListView(generic.ListView):
+    model = Expense
+    context_object_name = 'expenses'
+    template_name = 'expenses/expense_list.html'
+    ordering = ('-created_at',)
+
+
+@login_required
+def add_expense(request):
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.user = request.user
+            instance.save()
+            messages.success(request, _('Expense successfully added'))
+            return redirect('expenses:expense_list')
+        else:
+            messages.error(request, _('Invalid form'))
+            return redirect('expenses:expense_list')
+    else:
+        form = ExpenseForm()
+        return render(request, 'expenses/expense_form.html', {'form': form})
+
